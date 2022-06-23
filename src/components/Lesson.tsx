bae85660b3e0f42effef54ae2bef793cbac83803
Andre Kubotsu <contato201@gmail.com>
2022-06-23 09:41:48 -0300

    feat: Lesson component props and data

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,23 +1,37 @@
-import { CheckCircle } from "phosphor-react";
+import { CheckCircle, Lock } from "phosphor-react";
 
-export function Lesson() {
+interface LessonProps {
+  title: string;
+  slug: string;
+  availableAt: Date;
+  type: "live" | "class";
+}
+
+export function Lesson({ title, slug, availableAt, type }: LessonProps) {
+  const isLessonAvailable = false;
+
   return (
     <a href="">
-      <span className="text-gray-300">Domingo • 20 de junho • 19h00</span>
+      <span className="text-gray-300">{availableAt.toString()}</span>
 
       <div className="rounded border border-gray-500 p-4 mt-2">
         <header className="flex items-center justify-between">
-          <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
-            <CheckCircle size={20} />
-            Conteúdo liberado
-          </span>
+          {isLessonAvailable ? (
+            <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
+              <CheckCircle size={20} />
+              Conteúdo liberado
+            </span>
+          ) : (
+            <span className="text-sm text-orange-500 font-medium flex items-center gap-2">
+              <Lock size={20} />
+              Em breve
+            </span>
+          )}
           <span className="text-xs rounded px-2 py-[0.125rem] text-white border border-green-300 font-bold">
-            Ao vivo
+            {type === "live" ? "AO VIVO" : "AULA PRÁTICA"}
           </span>
         </header>
-        <strong className="text-gray-200 mt-5 block">
-          Abertura do evento Ignite labs
-        </strong>
+        <strong className="text-gray-200 mt-5 block">{title}</strong>
       </div>
     </a>
   );
